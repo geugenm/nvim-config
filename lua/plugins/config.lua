1a32eba829255c5a3a3ec6fcf514ff7bb69b0806
e_gleba <i@egleba.ru>
2025-01-18 14:58:18 +0300

    style fixes

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        cpp = { "clang-format" },
-        c = { "clang-format" },
-        python = { "isort", "black" },
-        rust = { "rustfmt" },
-        json = { "prettierd" },
-        cmake = { "cmake_format" },
-        yaml = { "prettierd", "prettier" },
-        javascript = { "prettierd", "prettier" },
-      },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                cpp = { "clang-format" },
+                c = { "clang-format" },
+                python = { "isort", "black" },
+                rust = { "rustfmt" },
+                json = { "prettierd" },
+                cmake = { "cmake_format" },
+                yaml = { "prettierd", "prettier" },
+                javascript = { "prettierd", "prettier" },
+            },
+        },
     },
-  },
 }
