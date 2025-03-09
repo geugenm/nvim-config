62757aac26432960e5fbe00d1caa788eafe52419
e_gleba <i@egleba.ru>
2025-03-09 20:55:32 +0300

    Update plugin versions and add new plugins

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -13,4 +13,44 @@ return {
             },
         },
     },
+    { "b0o/SchemaStore.nvim", lazy = true, version = false },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "yaml" },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = { ensure_installed = { "dockerfile" } },
+    },
+    { "mason.nvim", opts = { ensure_installed = { "hadolint" } } },
+    { "ThePrimeagen/refactoring.nvim" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "regex",
+                "vim",
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "shellcheck",
+                "shfmt",
+                "flake8",
+            },
+        },
+    },
+    { "Exafunction/codeium.nvim", lazy = true },
 }
