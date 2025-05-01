22bcd87599b3b71d97a866459de6129ade94ee1e
e_gleba <i@egleba.ru>
2025-05-01 22:22:09 +0300

    feat(lua): add mason.nvim and nvim-dap-python plugins

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,14 +1,33 @@
 return {
     {
-        "williamboman/mason.nvim",
+        'williamboman/mason.nvim',
         opts = {
             ensure_installed = {
-                "basedpyright", -- Fast Python type checker
-                "black", -- Python code formatter
-                "debugpy", -- Python debugger
-                "pylint", -- Python linter
-                "sonarlint-language-server", -- Static code analyzer
+                'basedpyright', -- Fast Python type checker
+                'black', -- Python code formatter
+                'debugpy', -- Python debugger
+                'pylint', -- Python linter
+                'sonarlint-language-server', -- Static code analyzer
             },
         },
+    },
+    {
+        'mfussenegger/nvim-dap-python',
+  -- stylua: ignore
+  keys = {
+    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+  },
+        config = function()
+            if vim.fn.has('win32') == 1 then
+                require('dap-python').setup(
+                    LazyVim.get_pkg_path('debugpy', '/venv/Scripts/pythonw.exe')
+                )
+            else
+                require('dap-python').setup(
+                    LazyVim.get_pkg_path('debugpy', '/venv/bin/python')
+                )
+            end
+        end,
     },
 }
