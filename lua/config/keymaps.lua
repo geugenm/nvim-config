3b79564f4229ca5b93ab712f431fe66356c6218f
e_gleba <i@egleba.ru>
2025-05-01 19:41:01 +0300

    feat(lua): Implement automated commit and push workflow

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,67 +3,110 @@
 -- Add any additional keymaps here
 
 local function lumen_commit_push()
-    -- Run lumen draft to generate commit message and capture the output
-    vim.fn.jobstart('lumen draft', {
-        stdout_buffered = true,
-        on_stdout = function(_, data)
-            if data and #data > 0 and data[1] ~= '' then
-                local commit_message = data[1]
+    local function log(msg, level)
+        vim.notify(msg, level or vim.log.levels.INFO)
+    end
 
-                -- Log the generated commit message
-                vim.notify(
-                    'lumen generated commit message: '
-                        .. vim.fn.shellescape(commit_message),
-                    vim.log.levels.INFO
-                )
+    local function run_cmd(cmd, opts)
+        opts = opts or {}
+        local stdout_data = {}
+        local stderr_data = {}
 
-                -- Execute git commit with the generated message
-                local commit_cmd = 'git commit -m '
-                    .. vim.fn.shellescape(commit_message)
-                vim.fn.jobstart(commit_cmd, {
-                    on_exit = function(_, exit_code)
-                        if exit_code == 0 then
-                            vim.notify('commit successful', vim.log.levels.INFO)
-
-                            -- Push the changes
-                            vim.fn.jobstart('git push', {
-                                on_exit = function(_, push_exit_code)
-                                    if push_exit_code == 0 then
-                                        vim.notify(
-                                            'push successful',
-                                            vim.log.levels.INFO
-                                        )
-                                    else
-                                        vim.notify(
-                                            'push failed with code: '
-                                                .. push_exit_code,
-                                            vim.log.levels.ERROR
-                                        )
-                                    end
-                                end,
-                            })
-                        else
-                            vim.notify(
-                                'commit failed with code: ' .. exit_code,
-                                vim.log.levels.ERROR
-                            )
+        vim.fn.jobstart(cmd, {
+            stdout_buffered = true,
+            on_stdout = function(_, data)
+                if data and #data > 0 then
+                    for _, line in ipairs(data) do
+                        if line ~= '' then
+                            table.insert(stdout_data, line)
                         end
+                    end
+                end
+            end,
+            on_stderr = function(_, data)
+                if data and #data > 0 then
+                    for _, line in ipairs(data) do
+                        if line ~= '' then
+                            table.insert(stderr_data, line)
+                        end
+                    end
+                end
+            end,
+            on_exit = function(_, code)
+                if code == 0 then
+                    if opts.on_success then
+                        opts.on_success(stdout_data)
+                    end
+                else
+                    local err = #stderr_data > 0
+                            and table.concat(stderr_data, '\n')
+                        or string.format('exited with code %d', code)
+                    if opts.on_error then
+                        opts.on_error(err)
+                    else
+                        log(
+                            string.format('%s failed: %s', opts.name, err),
+                            vim.log.levels.ERROR
+                        )
+                    end
+                end
+            end,
+        })
+    end
+
+    -- Generate commit message with lumen
+    run_cmd('lumen draft', {
+        name = 'lumen draft',
+        on_success = function(output)
+            if #output == 0 then
+                return log(
+                    'lumen draft produced no output',
+                    vim.log.levels.ERROR
+                )
+            end
+
+            local commit_message = output[1]
+            log(string.format('generated commit message: %s', commit_message))
+
+            -- Commit changes
+            run_cmd(
+                string.format(
+                    'git commit -m %s',
+                    vim.fn.shellescape(commit_message)
+                ),
+                {
+                    name = 'git commit',
+                    on_success = function()
+                        log('commit successful')
+
+                        -- Push changes
+                        run_cmd('git push', {
+                            name = 'git push',
+                            on_success = function()
+                                log('push successful')
+                            end,
+                            on_error = function(err)
+                                log(
+                                    string.format('git push failed: %s', err),
+                                    vim.log.levels.ERROR
+                                )
+                            end,
+                        })
                     end,
-                })
-            else
-                vim.notify(
-                    'failed to generate commit message',
-                    vim.log.levels.ERROR
-                )
-            end
+                    on_error = function(err)
+                        log(
+                            string.format('git commit failed: %s', err),
+                            vim.log.levels.ERROR
+                        )
+                    end,
+                }
+            )
         end,
-        on_stderr = function(_, data)
-            if data and #data > 0 and data[1] ~= '' then
-                vim.notify(
-                    'lumen error: ' .. table.concat(data, '\n'),
-                    vim.log.levels.ERROR
-                )
-            end
+        on_error = function(err)
+            log(
+                string.format('lumen draft failed: %s', err),
+                vim.log.levels.ERROR
+            )
         end,
     })
 end
